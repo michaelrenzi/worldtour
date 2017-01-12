02efc8141ab2eed24b38df8980289f3f0519c248
Michael Renzi <renzimichaelb@gmail.com>
2017-01-12 14:59:55 -0500

    added caption js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(function() {
     $('.pop').on('click', function() {
       $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+      document.getElementById("captionpreview").textContent=$(this).find('img').attr('alt');
       $('#imagemodal').modal('show');
     });
 });
